86c2431edc80f7da145df651641ec37a3c597fae
Lixy1997 <xyligoog2020@gmail.com>
2020-12-06 11:07:22 +0800

    Revert "Revert "update network and training""
    This reverts commit 8b80fbe11933c7a989a6e34997d0659285e3b2bf.

---
+++ train.py
@@ -0,0 +1,148 @@
+import sys
+import os
+import random
+import torch
+import argparse
+import numpy as np
+import time
+import shutil
+import json
+import my_optim
+import torch.optim as optim
+from models import vgg_gnn
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from torch.autograd import Variable
+from utils import AverageMeter
+from utils.LoadData import train_data_loader
+from tqdm import trange, tqdm
+
+ROOT_DIR = '/'.join(os.getcwd().split('/')[:-1])
+print('Project Root Dir:', ROOT_DIR)
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='group-wise semantic mining for weakly supervised semantic segmentation')
+    parser.add_argument("--root_dir", type=str, default=ROOT_DIR)
+    parser.add_argument("--img_dir", type=str, default='data/VOCdevkit/VOC2012/JPEGImages')
+    parser.add_argument("--train_list", type=str, default='data/VOCdevkit/VOC2012/ImageSets/Segmentation/train_cls.txt')
+    parser.add_argument("--test_list", type=str, default='data/VOCdevkit/VOC2012/ImageSets/Segmentation/val_cls.txt')
+    parser.add_argument("--num_classes", type=int, default=20) 
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--weight_decay", type=float, default=0.0005)
+    parser.add_argument("--input_size", type=int, default=256)
+    parser.add_argument("--crop_size", type=int, default=224)
+    parser.add_argument("--batch_size", type=int, default=1) 
+    parser.add_argument("--dataset", type=str, default='pascal_voc')
+    parser.add_argument("--drop_rate",type=float,default=0.95)
+    parser.add_argument("--drop_th",type=float,default=0.9)
+    parser.add_argument("--decay_points", type=str, default='5,10')
+    parser.add_argument("--snapshot_dir", type=str, default='runs/pascal/model')
+    parser.add_argument("--att_dir", type=str, default='runs/pascal/feat')
+    parser.add_argument("--threshold", type=float, default=0.6)
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser.add_argument("--disp_interval", type=int, default=100)
+    parser.add_argument("--resume", type=str, default='False')
+    parser.add_argument("--global_counter", type=int, default=0)
+    parser.add_argument("--epoch", type=int, default=15)
+    parser.add_argument("--current_epoch", type=int, default=0)
+
+    return parser.parse_args()
+
+def save_checkpoint(args, state, is_best, filename='checkpoint.pth.tar'):
+    savepath = os.path.join(args.snapshot_dir, filename)
+    torch.save(state, savepath)
+    if is_best:
+        shutil.copyfile(savepath, os.path.join(args.snapshot_dir, 'model_best.pth.tar'))
+
+def get_model(args):
+    model = gnn.GNNNet_VGG(pretrained=True,drop_rate=args.drop_rate,drop_th=args.drop_th, num_classes=args.num_classes,att_dir=args.att_dir, training_epoch=args.epoch)
+    device = torch.device(0)	
+    model = torch.nn.DataParallel(model).cuda()
+    model.to(device)
+    param_groups = model.module.get_parameter_groups()
+    optimizer = optim.SGD([
+        {'params': param_groups[0], 'lr': args.lr},
+        {'params': param_groups[1], 'lr': 2*args.lr},
+        {'params': param_groups[2], 'lr': 10*args.lr},
+        {'params': param_groups[3], 'lr': 20*args.lr}], momentum=0.9, weight_decay=args.weight_decay, nesterov=True)
+    return  model, optimizer
+
+def train(args):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    
+    total_epoch = args.epoch
+    global_counter = args.global_counter
+    current_epoch = args.current_epoch
+
+    train_loader, val_loader = train_data_loader(args)
+    max_step = total_epoch*len(train_loader)
+    args.max_step = max_step 
+    print('Max step:', max_step)
+    
+    model, optimizer = get_model(args)
+    
+    model.train()
+    print(model)
+    end = time.time()
+
+    while current_epoch < total_epoch:
+        model.train()
+        losses.reset()
+        batch_time.reset()
+        res = my_optim.reduce_lr(args, optimizer, current_epoch)
+        steps_per_epoch = len(train_loader)
+
+        index = 0  
+        for idx, dat in enumerate(train_loader):
+            
+            img_name1, img1, label1, img_name2, img2, label2, img_name3, img3, label3 = dat
+            label1 = label1.cuda(non_blocking=True)
+            label2 = label2.cuda(non_blocking=True)
+            label3 = label3.cuda(non_blocking=True)            
+            
+            x11, x1, x22,x2, x33,x3 = model(img1, img2, img3, current_epoch, label1, index)
+            index += 1
+
+            loss_train = 0.4 * (F.multilabel_soft_margin_loss(x11, label1) + F.multilabel_soft_margin_loss(x22, label2)
+                    + F.multilabel_soft_margin_loss(x33, label3)) + (F.multilabel_soft_margin_loss(x1, label1)
+                    + F.multilabel_soft_margin_loss(x2, label2) + F.multilabel_soft_margin_loss(x3, label3))
+
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+
+            losses.update(loss_train.data.item(), img.size()[0])
+            batch_time.update(time.time() - end)
+            end = time.time()
+            
+            global_counter += 1
+            if global_counter % 1000 == 0:
+                losses.reset()
+
+            if global_counter % args.disp_interval == 0:
+                print('Epoch: [{}][{}/{}]\t'
+                      'LR: {:.5f}\t' 
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                        current_epoch, global_counter%len(train_loader), len(train_loader), 
+                        optimizer.param_groups[0]['lr'], loss=losses))
+
+        if current_epoch == args.epoch-1:
+            save_checkpoint(args,
+                        {
+                            'epoch': current_epoch,
+                            'global_counter': global_counter,
+                            'state_dict':model.state_dict(),
+                            'optimizer':optimizer.state_dict()
+                        }, is_best=False,
+                        filename='%s_epoch_%d.pth' %(args.dataset, current_epoch))
+        current_epoch += 1
+
+if __name__ == '__main__':
+    args = get_arguments()
+    print('Running parameters:\n', args)
+    if not os.path.exists(args.snapshot_dir):
+        os.makedirs(args.snapshot_dir)
+    train(args)
---
+++ utils/transforms/transforms.py
@@ -0,0 +1,316 @@
+from __future__ import division
+import torch
+import math
+import random
+from PIL import Image, ImageOps, ImageEnhance
+try:
+    import accimage
+except ImportError:
+    accimage = None
+import numpy as np
+import numbers
+import types
+import collections
+import warnings
+
+from . import functional as F
+
+__all__ = ["Compose", "ToTensor", "ToPILImage", "Normalize", "Resize", "Scale", "CenterCrop", "Pad",
+           "Lambda", "RandomCrop", "RandomHorizontalFlip", "RandomVerticalFlip", "RandomResizedCrop",
+           "RandomSizedCrop", "FiveCrop", "TenCrop", "LinearTransformation", "ColorJitter", "RandomRotation",
+           "Grayscale", "RandomGrayscale"]
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        for t in self.transforms:
+            img = t(img)
+        return img
+
+
+class ToTensor(object):
+    def __call__(self, pic):
+        return F.to_tensor(pic)
+
+
+class ToPILImage(object):
+    def __init__(self, mode=None):
+        self.mode = mode
+
+    def __call__(self, pic):
+        return F.to_pil_image(pic, self.mode)
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        return F.normalize(tensor, self.mean, self.std)
+
+class Resize(object):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        assert isinstance(size, int) or (isinstance(size, collections.Iterable) and len(size) == 2)
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        return F.resize(img, (self.size, self.size), self.interpolation)
+
+
+class Scale(Resize):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("The use of the transforms.Scale transform is deprecated, " +
+                      "please use transforms.Resize instead.")
+        super(Scale, self).__init__(*args, **kwargs)
+
+
+class CenterCrop(object):
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, img):
+        return F.center_crop(img, self.size)
+
+
+class Pad(object):
+    def __init__(self, padding, fill=0):
+        assert isinstance(padding, (numbers.Number, tuple))
+        assert isinstance(fill, (numbers.Number, str, tuple))
+        if isinstance(padding, collections.Sequence) and len(padding) not in [2, 4]:
+            raise ValueError("Padding must be an int or a 2, or 4 element tuple, not a " +
+                             "{} element tuple".format(len(padding)))
+
+        self.padding = padding
+        self.fill = fill
+
+    def __call__(self, img):
+        return F.pad(img, self.padding, self.fill)
+
+
+class Lambda(object):
+    def __init__(self, lambd):
+        assert isinstance(lambd, types.LambdaType)
+        self.lambd = lambd
+
+    def __call__(self, img):
+        return self.lambd(img)
+
+
+class RandomCrop(object):
+    def __init__(self, size, padding=0):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+        self.padding = padding
+
+    @staticmethod
+    def get_params(img, output_size):
+        w, h = img.size
+        th, tw = output_size
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return i, j, th, tw
+
+    def __call__(self, img):
+        if self.padding > 0:
+            img = F.pad(img, self.padding)
+
+        i, j, h, w = self.get_params(img, self.size)
+
+        return F.crop(img, i, j, h, w)
+
+
+class RandomHorizontalFlip(object):
+    def __call__(self, img):
+        if random.random() < 0.5:
+            return F.hflip(img)
+        return img
+
+
+class RandomVerticalFlip(object):
+    def __call__(self, img):
+        if random.random() < 0.5:
+            return F.vflip(img)
+        return img
+
+
+class RandomResizedCrop(object):
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+        self.size = (size, size)
+        self.interpolation = interpolation
+        self.scale = scale
+        self.ratio = ratio
+
+    @staticmethod
+    def get_params(img, scale, ratio):
+        for attempt in range(10):
+            area = img.size[0] * img.size[1]
+            target_area = random.uniform(*scale) * area
+            aspect_ratio = random.uniform(*ratio)
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if random.random() < 0.5:
+                w, h = h, w
+
+            if w <= img.size[0] and h <= img.size[1]:
+                i = random.randint(0, img.size[1] - h)
+                j = random.randint(0, img.size[0] - w)
+                return i, j, h, w
+
+        w = min(img.size[0], img.size[1])
+        i = (img.size[1] - w) // 2
+        j = (img.size[0] - w) // 2
+        return i, j, w, w
+
+    def __call__(self, img):
+        i, j, h, w = self.get_params(img, self.scale, self.ratio)
+        return F.resized_crop(img, i, j, h, w, self.size, self.interpolation)
+
+
+class RandomSizedCrop(RandomResizedCrop):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("The use of the transforms.RandomSizedCrop transform is deprecated, " +
+                      "please use transforms.RandomResizedCrop instead.")
+        super(RandomSizedCrop, self).__init__(*args, **kwargs)
+
+
+class FiveCrop(object):
+    def __init__(self, size):
+        self.size = size
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            assert len(size) == 2, "Please provide only two dimensions (h, w) for size."
+            self.size = size
+
+    def __call__(self, img):
+        return F.five_crop(img, self.size)
+
+
+class TenCrop(object):
+    def __init__(self, size, vertical_flip=False):
+        self.size = size
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            assert len(size) == 2, "Please provide only two dimensions (h, w) for size."
+            self.size = size
+        self.vertical_flip = vertical_flip
+
+    def __call__(self, img):
+        return F.ten_crop(img, self.size, self.vertical_flip)
+
+
+class LinearTransformation(object):
+    def __init__(self, transformation_matrix):
+        if transformation_matrix.size(0) != transformation_matrix.size(1):
+            raise ValueError("transformation_matrix should be square. Got " +
+                             "[{} x {}] rectangular matrix.".format(*transformation_matrix.size()))
+        self.transformation_matrix = transformation_matrix
+
+    def __call__(self, tensor):
+        if tensor.size(0) * tensor.size(1) * tensor.size(2) != self.transformation_matrix.size(0):
+            raise ValueError("tensor and transformation matrix have incompatible shape." +
+                             "[{} x {} x {}] != ".format(*tensor.size()) +
+                             "{}".format(self.transformation_matrix.size(0)))
+        flat_tensor = tensor.view(1, -1)
+        transformed_tensor = torch.mm(flat_tensor, self.transformation_matrix)
+        tensor = transformed_tensor.view(tensor.size())
+        return tensor
+
+
+class ColorJitter(object):
+    def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+    @staticmethod
+    def get_params(brightness, contrast, saturation, hue):
+        transforms = []
+        if brightness > 0:
+            brightness_factor = np.random.uniform(max(0, 1 - brightness), 1 + brightness)
+            transforms.append(Lambda(lambda img: F.adjust_brightness(img, brightness_factor)))
+
+        if contrast > 0:
+            contrast_factor = np.random.uniform(max(0, 1 - contrast), 1 + contrast)
+            transforms.append(Lambda(lambda img: F.adjust_contrast(img, contrast_factor)))
+
+        if saturation > 0:
+            saturation_factor = np.random.uniform(max(0, 1 - saturation), 1 + saturation)
+            transforms.append(Lambda(lambda img: F.adjust_saturation(img, saturation_factor)))
+
+        if hue > 0:
+            hue_factor = np.random.uniform(-hue, hue)
+            transforms.append(Lambda(lambda img: F.adjust_hue(img, hue_factor)))
+
+        np.random.shuffle(transforms)
+        transform = Compose(transforms)
+
+        return transform
+
+    def __call__(self, img):
+        transform = self.get_params(self.brightness, self.contrast,
+                                    self.saturation, self.hue)
+        return transform(img)
+
+
+class RandomRotation(object):
+    def __init__(self, degrees, resample=False, expand=False, center=None):
+        if isinstance(degrees, numbers.Number):
+            if degrees < 0:
+                raise ValueError("If degrees is a single number, it must be positive.")
+            self.degrees = (-degrees, degrees)
+        else:
+            if len(degrees) != 2:
+                raise ValueError("If degrees is a sequence, it must be of len 2.")
+            self.degrees = degrees
+
+        self.resample = resample
+        self.expand = expand
+        self.center = center
+
+    @staticmethod
+    def get_params(degrees):
+        angle = np.random.uniform(degrees[0], degrees[1])
+
+        return angle
+
+    def __call__(self, img):
+        angle = self.get_params(self.degrees)
+
+        return F.rotate(img, angle, self.resample, self.expand, self.center)
+
+
+class Grayscale(object):
+    def __init__(self, num_output_channels=1):
+        self.num_output_channels = num_output_channels
+
+    def __call__(self, img):
+        return F.to_grayscale(img, num_output_channels=self.num_output_channels)
+
+
+class RandomGrayscale(object):
+    def __init__(self, p=0.1):
+        self.p = p
+
+    def __call__(self, img):
+        num_output_channels = 1 if img.mode == 'L' else 3
+        if random.random() < self.p:
+            return F.to_grayscale(img, num_output_channels=num_output_channels)
+        return img
